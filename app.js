b5fa6458d098af56ff5e06aace5631b08e270fd0
logesh2496 <you@example.com>
2021-11-28 22:14:45 +0530

    fix: add cors

--- app.js
+++ app.js
@@ -4,8 +4,12 @@ const { Requester } = require('@chainlink/external-adapter')
 
 const express = require('express')
 const bodyParser = require('body-parser')
+var cors = require('cors')
+
 const app = express()
 const port = process.env.PORT || 8080
+
+app.use(cors())
 
 app.use(bodyParser.json())
 
